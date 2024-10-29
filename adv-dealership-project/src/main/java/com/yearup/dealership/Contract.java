5aa636bdb62000aee5df0e106247d5e4c0854865
poteatc <poteatc@outlook.com>
2024-10-29 09:04:04 -0400

    Implemented fields, constructor, getters/setters, and abstract methods for Contract

--- adv-dealership-project/src/main/java/com/yearup/dealership/Contract.java
+++ adv-dealership-project/src/main/java/com/yearup/dealership/Contract.java
@@ -1,4 +1,54 @@
 package com.yearup.dealership;
 
 public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private boolean vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String date, String customerName, String customerEmail, boolean vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    // Abstract methods (cannot be private) will return computed values based on contract type
+    abstract double getTotalPrice();
+
+    abstract double getMonthlyPayment();
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean isVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(boolean vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
 }

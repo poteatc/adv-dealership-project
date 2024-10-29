5dd33b983bfe5c7fb7afe7e128c089e06af5a620
poteatc <poteatc@outlook.com>
2024-10-29 09:21:39 -0400

    added constructors and all method stubs for instance variables in SalesContract and LeaseContract

--- adv-dealership-project/src/main/java/com/yearup/dealership/LeaseContract.java
+++ adv-dealership-project/src/main/java/com/yearup/dealership/LeaseContract.java
@@ -1,4 +1,21 @@
 package com.yearup.dealership;
 
 public class LeaseContract extends Contract {
+
+    private final double expectedEndingValue = .5;
+    private final double leaseFee = .07;
+
+    public LeaseContract(String date, String customerName, String customerEmail, boolean vehicleSold) {
+        super(date, customerName, customerEmail, vehicleSold);
+    }
+
+    @Override
+    double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    double getMonthlyPayment() {
+        return 0;
+    }
 }
--- adv-dealership-project/src/main/java/com/yearup/dealership/SalesContract.java
+++ adv-dealership-project/src/main/java/com/yearup/dealership/SalesContract.java
@@ -1,4 +1,50 @@
 package com.yearup.dealership;
 
 public class SalesContract extends Contract {
+
+    private final double salesTaxAmount = .05;
+    private final int recordingFee = 100;
+    private int processingFee;
+    private boolean financing;
+
+    public SalesContract(String date, String customerName, String customerEmail, boolean vehicleSold, int processingFee, boolean financing) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.processingFee = processingFee;
+        this.financing = financing;
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+
+    public int getRecordingFee() {
+        return recordingFee;
+    }
+
+    public int getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(int processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinancing() {
+        return financing;
+    }
+
+    public void setFinancing(boolean financing) {
+        this.financing = financing;
+    }
+
+    @Override
+    double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    double getMonthlyPayment() {
+        return 0;
+    }
 }
